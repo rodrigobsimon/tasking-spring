28ca82b22e8d161244a2482e9b85422e5ecb4ed9
Rodrigo Simon <rodrigobsimon@hotmail.com>
2022-10-23 18:55:43 -0300

    feat: add toString and equalsHashCode methods

--- src/main/java/com/github/rodrigobsimon/tasking/task/domain/Task.java
+++ src/main/java/com/github/rodrigobsimon/tasking/task/domain/Task.java
@@ -1,17 +1,21 @@
 package com.github.rodrigobsimon.tasking.task.domain;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @RequiredArgsConstructor
 @Getter
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Task {
 
-	private final String id = UUID.randomUUID()
-	                              .toString();
+	@EqualsAndHashCode.Include private final String id = UUID.randomUUID()
+	                                                         .toString();
 	private final String description;
 	private final LocalDateTime creationMoment = LocalDateTime.now();
 	private TaskStatus status = TaskStatus.TODO;
