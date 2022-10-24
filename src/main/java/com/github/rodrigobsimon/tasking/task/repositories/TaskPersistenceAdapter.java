0d72d358a651dd1eb9c72e3be03e6839b2ab41ae
Rodrigo Simon <rodrigobsimon@hotmail.com>
2022-10-24 10:43:52 -0300

    feat: add implementation to BuildTaskBoardPort

--- src/main/java/com/github/rodrigobsimon/tasking/task/repositories/TaskPersistenceAdapter.java
+++ src/main/java/com/github/rodrigobsimon/tasking/task/repositories/TaskPersistenceAdapter.java
@@ -1,13 +1,21 @@
 package com.github.rodrigobsimon.tasking.task.repositories;
 
 import com.github.rodrigobsimon.tasking.task.domain.Task;
+import com.github.rodrigobsimon.tasking.task.domain.TaskBoardItem;
+import com.github.rodrigobsimon.tasking.task.domain.TaskStatus;
 import com.github.rodrigobsimon.tasking.task.ports.AddTaskPort;
+import com.github.rodrigobsimon.tasking.task.ports.BuildTaskBoardPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @Component
 @RequiredArgsConstructor
-public class TaskPersistenceAdapter implements AddTaskPort {
+public class TaskPersistenceAdapter implements AddTaskPort,
+                                               BuildTaskBoardPort {
 
 	private final TaskRepository taskRepository;
 
@@ -17,5 +25,13 @@ public class TaskPersistenceAdapter implements AddTaskPort {
 		TaskEntity taskEntity =
 				new TaskEntity(task.getId(), task.getDescription(), task.getCreationMoment(), task.getStatus());
 		taskRepository.save(taskEntity);
+	}
+
+	@Override
+	public Map<TaskStatus, List<TaskBoardItem>> buildTaskBoard() {
+
+		return taskRepository.findAll()
+		                     .stream()
+		                     .collect(Collectors.groupingBy(TaskBoardItem::getStatus));
 	}
 }
