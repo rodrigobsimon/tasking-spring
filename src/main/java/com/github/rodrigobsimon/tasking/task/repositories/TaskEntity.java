018c0a0e4c3f3ed35de36218775eade6a89debd6
Rodrigo Simon <rodrigobsimon@hotmail.com>
2022-10-24 10:43:33 -0300

    feat: add implementation to TaskBoardItem

--- src/main/java/com/github/rodrigobsimon/tasking/task/repositories/TaskEntity.java
+++ src/main/java/com/github/rodrigobsimon/tasking/task/repositories/TaskEntity.java
@@ -1,7 +1,9 @@
 package com.github.rodrigobsimon.tasking.task.repositories;
 
+import com.github.rodrigobsimon.tasking.task.domain.TaskBoardItem;
 import com.github.rodrigobsimon.tasking.task.domain.TaskStatus;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
@@ -9,19 +11,20 @@ import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED, onConstructor = @__(@Deprecated))
-public class TaskEntity {
+@Getter
+public class TaskEntity implements TaskBoardItem {
 
-	@Id @GeneratedValue(strategy = GenerationType.SEQUENCE) private Long id;
-	private String internalId;
+	@Id @GeneratedValue(strategy = GenerationType.SEQUENCE) private Long internalId;
+	private String id;
 	private String description;
 	private LocalDateTime creationMoment;
-	@Enumerated(EnumType.STRING) private TaskStatus taskStatus;
+	@Enumerated(EnumType.STRING) private TaskStatus status;
 
-	public TaskEntity(String internalId, String description, LocalDateTime creationMoment, TaskStatus taskStatus) {
+	public TaskEntity(String id, String description, LocalDateTime creationMoment, TaskStatus status) {
 
-		this.internalId = internalId;
+		this.id = id;
 		this.description = description;
 		this.creationMoment = creationMoment;
-		this.taskStatus = taskStatus;
+		this.status = status;
 	}
 }
