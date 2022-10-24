86ee0922414311397a68b7ad324acd32cfa38b4c
Rodrigo Simon <rodrigobsimon@hotmail.com>
2022-10-24 10:42:58 -0300

    feat: add step to refresh task board

--- src/main/java/com/github/rodrigobsimon/tasking/task/controllers/SubmitFormAddTaskController.java
+++ src/main/java/com/github/rodrigobsimon/tasking/task/controllers/SubmitFormAddTaskController.java
@@ -2,6 +2,8 @@ package com.github.rodrigobsimon.tasking.task.controllers;
 
 import com.github.rodrigobsimon.tasking.task.controllers.requests.AddTaskRequest;
 import com.github.rodrigobsimon.tasking.task.domain.Task;
+import com.github.rodrigobsimon.tasking.task.domain.TaskStatus;
+import com.github.rodrigobsimon.tasking.task.ports.BuildTaskBoardPort;
 import com.github.rodrigobsimon.tasking.task.usecases.AddTaskUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -16,11 +18,17 @@ import javax.validation.Valid;
 public class SubmitFormAddTaskController {
 
 	private final AddTaskUseCase addTaskUseCase;
+	private final BuildTaskBoardPort buildTaskBoardPort;
 
 	@PostMapping(path = "/tasks/add-task")
 	public String addTask(@ModelAttribute @Valid AddTaskRequest request, Model model) {
 
+		var board = buildTaskBoardPort.buildTaskBoard();
+
 		addTaskUseCase.execute(new Task(request.getDescription()));
+		model.addAttribute("todos", board.get(TaskStatus.TODO));
+		model.addAttribute("doings", board.get(TaskStatus.DOING));
+		model.addAttribute("dones", board.get(TaskStatus.DONE));
 		return "tasks";
 	}
 }
